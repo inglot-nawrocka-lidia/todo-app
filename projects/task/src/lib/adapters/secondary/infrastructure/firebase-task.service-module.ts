b56293bae91ab67882bebbd5aa05461c1b31731c
inglot-nawrocka-lidia <inglot.lidia@gmail.com>
2022-04-28 11:53:09 +0200

    add Dto port and firebase

--- projects/task/src/lib/adapters/secondary/infrastructure/firebase-task.service-module.ts
+++ projects/task/src/lib/adapters/secondary/infrastructure/firebase-task.service-module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { FirebaseTaskService } from './firebase-task.service';
+import { ADDS_TASK_DTO } from '../../../application/ports/secondary/adds-task.dto-port';
+import { GETS_ALL_TASK_DTO } from '../../../application/ports/secondary/gets-all-task.dto-port';
+import { REMOVES_TASK_DTO } from '../../../application/ports/secondary/removes-task.dto-port';
+import { SETS_TASK_DTO } from '../../../application/ports/secondary/sets-task.dto-port';
 
-@NgModule({ imports: [AngularFirestoreModule],
+@NgModule({ 
+	imports: [AngularFirestoreModule],
   	declarations: [],
-  	providers: [FirebaseTaskService],
+  	providers: [FirebaseTaskService, 
+		{ provide: ADDS_TASK_DTO, useExisting: FirebaseTaskService },  
+		{ provide: GETS_ALL_TASK_DTO, useExisting: FirebaseTaskService }, 
+		{ provide: REMOVES_TASK_DTO, useExisting: FirebaseTaskService },
+		{ provide: SETS_TASK_DTO, useExisting: FirebaseTaskService }],
   	exports: [] })
 export class FirebaseTaskServiceModule {
 }
--- projects/task/src/lib/adapters/secondary/infrastructure/firebase-task.service.ts
+++ projects/task/src/lib/adapters/secondary/infrastructure/firebase-task.service.ts
@@ -1,7 +1,34 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AddsTaskDtoPort } from '../../../application/ports/secondary/adds-task.dto-port';
+import { TaskDTO } from '../../../application/ports/secondary/task.dto';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { GetsAllTaskDtoPort } from '../../../application/ports/secondary/gets-all-task.dto-port';
+import { filterByCriterion } from '@lowgular/shared';
+import { RemovesTaskDtoPort } from '../../../application/ports/secondary/removes-task.dto-port';
+import { SetsTaskDtoPort } from '../../../application/ports/secondary/sets-task.dto-port';
 
 @Injectable()
-export class FirebaseTaskService {
+export class FirebaseTaskService implements AddsTaskDtoPort, GetsAllTaskDtoPort, RemovesTaskDtoPort, SetsTaskDtoPort {
   constructor(private _client: AngularFirestore) {}
+
+  add(task: Partial<TaskDTO>): void {
+    this._client.collection('task').add(task);
+  }
+
+  getAll(criterion: Partial<TaskDTO>): Observable<TaskDTO[]> {
+    return this._client
+    .collection<TaskDTO>('task')
+    .valueChanges(({idField: 'id'}))
+    .pipe(map((data: TaskDTO[]) => filterByCriterion(data, criterion)));
+  }
+
+  remove(id: string): void {
+    this._client.doc('task/'+id).delete();
+  }
+
+  set(task: Partial<TaskDTO>): void {
+    this._client.doc('task/'+task.id).update(task);
+  }
 }
